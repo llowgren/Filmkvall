f793c0c18ec4cd63946de1701c43dfeef975669b
llowgren <61126248+llowgren@users.noreply.github.com>
2025-12-26 15:46:37 +0100

    Update film-system.js

--- film-system.js
+++ film-system.js
@@ -1,25 +1,51 @@
-import { getAuth } from './store.js';
+// film-system.js
+import { getWho, getAuth, on } from './store.js';
 import { getApiUrl, getMovieTokens } from './film-login.js';
 
 customElements.define('film-system', class extends HTMLElement {
-  connectedCallback(){ this.render(); }
+  connectedCallback() {
+    this.render();
+    this.unsubWho = on('who', () => this.render());
+    this.unsubAuth = on('auth', () => this.render());
+  }
 
-  render(){
+  disconnectedCallback() {
+    this.unsubWho?.();
+    this.unsubAuth?.();
+  }
+
+  render() {
+    const who = getWho();
     const auth = getAuth();
-    const url = getApiUrl();
-    const t = getMovieTokens();
+    const apiUrl = getApiUrl();
+    const tokens = getMovieTokens();
+
+    const view = {
+      who,
+      apiUrl,
+      auth: {
+        pw: auth.pw ? '(satt)' : '',
+        token: auth.token ? '(satt)' : ''
+      },
+      tokens: {
+        tmdb: !!tokens.tmdb,
+        omdb: !!tokens.omdb,
+        watchmode: !!tokens.watchmode
+      }
+    };
 
     this.innerHTML = `
       <div class="card">
         <h3>System</h3>
         <div class="muted" style="font-size:13px">Felsökning/status</div>
-        <pre style="white-space:pre-wrap">${escapeHtml(JSON.stringify({
-          apiUrl: url,
-          auth: { pw: auth.pw ? '(satt)' : '', token: auth.token ? '(satt)' : '' },
-          tokens: { tmdb: !!t.tmdb, omdb: !!t.omdb, watchmode: !!t.watchmode }
-        }, null, 2))}</pre>
-      </div>`;
+        <pre style="white-space:pre-wrap;margin:10px 0 0">${escapeHtml(JSON.stringify(view, null, 2))}</pre>
+      </div>
+    `;
   }
 });
 
-function escapeHtml(s){ return String(s).replace(/[&<>"']/g, m=>({ '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;' }[m])); }
+function escapeHtml(s){
+  return String(s).replace(/[&<>"']/g, m => ({
+    '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'
+  }[m]));
+}
